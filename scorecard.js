5fbb61a6f99f116723101af3b09e55c02b24451f
jwebber92 <joewebber.92@gmail.com>
2019-06-18 11:50:04 +0100

    Scorecard actually looks like a scorecard

--- scorecard.js
+++ scorecard.js
@@ -3,39 +3,50 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Table, Row, Cell} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 type Props = {};
 export default class Scorecard extends Component<Props> {
+  renderColumn(column) {
+    return (
+      <View style={styles.cell}>
+        <Text style={styles.comment}>{column}</Text>
+      </View>
+    )
+  }
+
+  renderRow(row) {
+    return (
+      <View style={styles.row}>
+        {
+          row.map((column) => {
+            return this.renderColumn(column);
+          })
+        }
+      </View>
+    );
+  }
+
   render() {
+    const scores = [
+      ['Hole', 'Yards', 'Par', 'Player 1', 'Player 2'],
+      [1, 140, 3, 4, 5],
+      [2, 250, 4, 4, 6],
+      [3, 160, 3, 3, 3],
+      [4, 300, 5, 5, 6],
+      [5, 270, 4, 7, 3]
+     ];
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Scorecard</Text>
-        <Text style={styles.comment}>To get started, edit App.js</Text>
-        <Table style={styles.scorecard}>
-          <Row>
-            <Cell>
-              <Text style={styles.comment}>Joe</Text>
-            </Cell>
-            <Cell>
-              <Text style={styles.comment}>3</Text>
-            </Cell>
-            <Cell>
-              <Text style={styles.comment}>4</Text>
-            </Cell>
-            <Cell>
-              <Text style={styles.comment}>4</Text>
-            </Cell>
-            <Cell>
-              <Text style={styles.comment}>3</Text>
-            </Cell>
-            <Cell>
-              <Text style={styles.comment}>7</Text>
-            </Cell>
-          </Row>
-        </Table>
+        <Text style={styles.comment}>This is the scorecard!</Text>
+        {
+          scores.map((score) => {
+            return this.renderRow(score);
+          })
+        }
       </View>
-    );
+    )
   }
 }
 
@@ -59,5 +70,13 @@ const styles = StyleSheet.create({
   scorecard: {
     fontSize: 14,
     textAlign: 'left',
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  cell: {
+    flexDirection: 'column',
+    padding: 10,
   },
 });
