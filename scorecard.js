b70ce5ec1134c5f472a585d5fb6e9032cd9a6e11
jwebber92 <joewebber.92@gmail.com>
2019-06-21 15:41:44 +0100

    Scorecard - swapped columns with rows to reflect standard layout

--- scorecard.js
+++ scorecard.js
@@ -6,35 +6,39 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import scores from './scores.json';
 
-const maxHoles = 4;
+const numberOfPlayers = 2;
 
 type Props = {};
 export default class Scorecard extends Component<Props> {
-  renderHolesRow() {
-    let holesNo = [];
-    for (let i=1; i<maxHoles; i++) {
-      holesNo.push(<Text style={styles.cell}>{i}</Text>)
+  renderTitleRow() {
+    let playerNames = [];
+    for (let i=1; i<numberOfPlayers+1; i++) {
+      playerNames.push(<Text style={styles.column}>{"Player "+i}</Text>)
     }
 
     return (
-      <View style={styles.row} key="holeNo">
-        <Text style={styles.cell}>Hole</Text>
-        {holesNo}
+      <View style={styles.row} key="titleRow">
+        <Text style={styles.column}>Hole</Text>
+        <Text style={styles.column}>Par</Text>
+        <Text style={styles.column}>Yards</Text>
+        {playerNames}
       </View>
     )
   }
 
   renderRow(row) {
     // Using array might cause 'key' prop errors
-    let cells = [];
-    for (let i=1; i<maxHoles; i++) {
-      cells.push(<Text style={styles.cell}>{row["hole"+i]}</Text>)
+    let playerScores = [];
+    for (let i=1; i<numberOfPlayers+1; i++) {
+      playerScores.push(<Text style={styles.column}>{row.data["player"+i]}</Text>)
     }
 
     return (
-      <View style={styles.row} key={row.id}>
-        <Text style={styles.cell}>{row.id}</Text>
-        {cells}
+      <View style={styles.row} key={row.holeNumber}>
+        <Text style={styles.column}>{row.holeNumber}</Text>
+        <Text style={styles.column}>{row.data.par}</Text>
+        <Text style={styles.column}>{row.data.yards}</Text>
+        {playerScores}
       </View>
     );
   }
@@ -45,7 +49,7 @@ export default class Scorecard extends Component<Props> {
         <Text style={styles.title}>Scorecard</Text>
         <Text style={styles.comment}>Course: My Favourite Green</Text>
         <Text style={styles.comment}>Date: 1st January 2000</Text>
-        {this.renderHolesRow()}
+        {this.renderTitleRow()}
         {
           scores.map((score) => {
             return this.renderRow(score);
@@ -78,11 +82,16 @@ const styles = StyleSheet.create({
     textAlign: 'left',
   },
   row: {
+    display: 'flex',
     flexDirection: 'row',
+//    width: '100%',
     padding: 5,
   },
-  cell: {
+  column: {
+    display: 'flex',
     flexDirection: 'column',
+//    flexBasis: '100%',
+//    flex: 1,
     padding: 10,
   },
 });
